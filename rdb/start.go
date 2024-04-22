cf212c8232c81cdab1b69d7a1a4d8b46c2c9ab54
jixiangup <jixiangup@gmail.com>
2024-04-22 12:09:39 +0800

    refactor: remove credentials required

--- rdb/start.go
+++ rdb/start.go
@@ -22,28 +22,30 @@ type Connection struct {
 
 func Run() (*ExecCommand, error) {
 	var action string
-	var connJson string
+	var credentials string
 	var webhookURL string
 	var secret string
 
 	flag.StringVar(&action, "action", "", "API action to perform")
-	flag.StringVar(&connJson, "credentials", "", "Connection credentials in JSON format")
+	flag.StringVar(&credentials, "credentials", "", "Connection credentials in JSON format")
 	flag.StringVar(&webhookURL, "webhook", "", "Webhook URL to post results")
 	flag.StringVar(&secret, "secret", "", "Secret key for webhook authentication")
 	flag.Parse()
 
 	// 解析连接信息
 	var conn Connection
-	if err := json.Unmarshal(
-		[]byte(connJson),
-		&conn,
-	); err != nil {
-		return nil, fmt.Errorf("error parsing credentials json: %s", err)
+	if credentials != "" {
+		if err := json.Unmarshal(
+			[]byte(credentials),
+			&conn,
+		); err != nil {
+			return nil, fmt.Errorf("error parsing credentials json: %s", err)
+		}
 	}
 
 	return &ExecCommand{
 		Action:      action,
-		Credentials: connJson,
+		Credentials: credentials,
 		Webhook:     webhookURL,
 		Secret:      secret,
 	}, nil
