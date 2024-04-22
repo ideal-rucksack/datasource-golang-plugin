aca72c39a255a36dc4b8e6a0cf5434a0e19a06f3
jixiangup <jixiangup@gmail.com>
2024-04-22 12:39:22 +0800

    fixed: command interface api response

--- rdb/command.go
+++ rdb/command.go
@@ -2,7 +2,7 @@ package rdb
 
 type Command interface {
 	// Execute 执行命令
-	Execute() (*Result, error)
+	Execute() (Result, error)
 	// Notify 回调通知调用方
 	// args 为执行命令的参数
 	// payload 为执行结果作为POST请求的body(请求体)
